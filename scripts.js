6af4162d525585f2b152f97c74071c36d3381b05
Alex-Dev5001 <allexjsa@gmail.com>
2026-03-29 16:05:15 -0300

    web page complete

---
+++ scripts.js
@@ -0,0 +1,10 @@
+function drawNumbers() {
+const min = document.querySelector(".input-min").value;
+const max = document.querySelector(".input-max").value;
+const result = Math.floor(Math.random() * (max - min + 1)) + parseInt(min);
+resultElement.value = result;
+}
+const resultElement = document.querySelector(".result");
+console.log(resultElement.value)
+const buttonSort = document.querySelector("button");
+buttonSort.addEventListener("click", drawNumbers);
