44c58f86028b83cf5783e99ce798f309795dcdf5
Alex-Dev5001 <allexjsa@gmail.com>
2026-03-31 21:16:05 -0300

    design rule JS

--- scripts.js
+++ scripts.js
@@ -1,13 +1,18 @@
 function drawNumbers() {
-const min = document.querySelector(".input-min").value;
-const max = document.querySelector(".input-max").value;
-const result = Math.floor(Math.random() * (max - min + 1)) + parseInt(min);
-resultElement.value = result;
+    const min = document.querySelector(".input-min").value;
+    const max = document.querySelector(".input-max").value;
+    const result = Math.floor(Math.random() * (max - min + 1)) + parseInt(min);
+    resultElement.value = result;
+    if (min >= max) {
+        alert("O valor mínimo deve ser menor que o valor máximo.")
+    }
 }
 const resultElement = document.querySelector(".result");
 console.log(resultElement.value)
 const buttonSort = document.querySelector("button");
 buttonSort.addEventListener("click", drawNumbers);
+
+
 
 const larguraJanela = window.innerWidth;
 const alturaJanela = window.innerHeight;
